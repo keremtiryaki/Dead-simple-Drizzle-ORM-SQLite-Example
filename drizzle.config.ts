57e0efab06cc37ca91de78e1f4f06ea58a1c87c6
kerem <keremtiryaki@gmail.com>
2023-07-12 18:17:29 -0400

    sqlite http connection with sqld https://github.com/libsql/sqld/blob/main/docs/BUILD-RUN.md#using-a-prebuilt-docker-image

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+    schema: "./src/db/schema/*",
+    out: "./dkit-migrations-folder",
+    // driver: 'better-sqlite',
+    // dbCredentials: {
+    //     url: 'todos.db'
+    // }
+    driver: 'libsql',
+    dbCredentials: {
+        url: 'http://localhost:8080'
+    }
+} satisfies Config;
+
